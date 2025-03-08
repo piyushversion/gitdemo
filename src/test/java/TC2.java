c103b119262fd8b085476644a98fbaa4c406d936
Your Name <you@example.com>
2025-03-07 22:34:00 -0800

    1.0

---
+++ src/test/java/TC2.java
@@ -0,0 +1,15 @@
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.Assert.*;
+
+public class TC2 {
+
+	@Test
+	public void test() {
+		
+		System.out.println("Hello Piyush");
+		
+	}
+
+}
